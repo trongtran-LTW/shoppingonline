ba734733857546335ab4919aa4e6cd855936aa6e
trongtran-LTW <trongthichhoa1234@gmail.com>
2024-07-16 09:36:54 +0700

    Add files via upload

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+  DB_SERVER: 'sgpcluster.gizbzd0.mongodb.net',
+  DB_USER: 'trong',
+  DB_PASS: 'trong1423',
+  DB_DATABASE: 'shoppingonline',
+  JWT_SECRET: '<jwt_secret>',
+  JWT_EXPIRES: '360000000000000000', // in milliseconds
+  EMAIL_USER: 'ttnsonic@gmail.com', // gmail service
+  EMAIL_PASS: 'qbhc yokt tcgs yklp'
+};
+module.exports = MyConstants;
